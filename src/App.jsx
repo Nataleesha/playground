28ef75615f25118c8424ab46c0977a262df501e0
Nataleesha <frojlan@gmail.com>
2023-11-02 00:17:07 +0200

    rock paper scissors raw

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 import { HelmetProvider } from "react-helmet-async";
 
-// import Header from "/src/components/Header";
-// import Home from "/src/pages/Home/Home";
-
 const Header = lazy(() => import("/src/components/Header.jsx"));
 const Menu = lazy(() => import("/src/pages/Menu/Menu.jsx"));
+const RockPaperScissors = lazy(() =>
+  import("/src/pages/RockPaperScissors/RockPaperScissors.jsx")
+);
 
 const App = () => {
   return (
@@ -15,10 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Header />}>
             <Route index element={<Menu />} />
-            <Route
-              path="rock-paper-scissors"
-              element={"Rock, Paper, Scissors Game"}
-            />
+            <Route path="rock-paper-scissors" element={<RockPaperScissors />} />
           </Route>
         </Routes>
       </div>
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,12 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Menu = () => {
   return (
-    <ul>
-      <li>
-        <NavLink to="rock-paper-scissors">Rock, Paper, Scissors</NavLink>
-      </li>
-    </ul>
+    <div>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <ul>
+        <li>
+          <NavLink to="rock-paper-scissors">Rock, Paper, Scissors</NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 
